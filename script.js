789aff66d33df21bd91a1407dec2d2f651cfdec5
tamimd200 <162560149+tamimd200@users.noreply.github.com>
2024-03-07 10:54:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var countdownElement = document.getElementById('countdown');
+
+    // Set the date and time to countdown to (YYYY-MM-DD HH:MM:SS)
+    var countDownDate = new Date('2024-03-07T16:00:00').getTime();
+
+    var x = setInterval(function () {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        countdownElement.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+        if (distance < 0) {
+            clearInterval(x);
+            countdownElement.innerHTML = "EXPIRED";
+        }
+    }, 1000);
+});
